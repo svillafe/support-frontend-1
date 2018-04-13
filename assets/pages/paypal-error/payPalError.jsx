6cd0b13bb9da13556c52ca2276e9238708cf8a2c
James Byers <Ap0c@users.noreply.github.com>
2018-04-13 18:37:09 +0100

    Refactored the PayPal error page to use the new components.

--- assets/pages/paypal-error/payPalError.jsx
+++ assets/pages/paypal-error/payPalError.jsx
@@ -6,9 +6,10 @@ import React from 'react';
 
 import SimpleHeader from 'components/headers/simpleHeader/simpleHeader';
 import Footer from 'components/footer/footer';
+import QuestionsContact from 'components/questionsContact/questionsContact';
+import ErrorIntroduction from 'components/errorIntroduction/errorIntroduction';
+import PageSection from 'components/pageSection/pageSection';
 import CtaLink from 'components/ctaLink/ctaLink';
-import InfoSection from 'components/infoSection/infoSection';
-import SocialShare from 'components/socialShare/socialShare';
 
 import { statelessInit as pageInit } from 'helpers/page/page';
 import { renderPage } from 'helpers/render';
@@ -24,39 +25,20 @@ pageInit();
 const content = (
   <div className="gu-content">
     <SimpleHeader />
-    <section className="paypal-error gu-content-filler">
-      <div className="paypal-error__content gu-content-filler__inner">
-        <div className="paypal-error__wrapper">
-          <h1 className="paypal-error__heading">PayPal Error!</h1>
-          <h2 className="paypal-error__subheading">
-            <p>Sorry, there was a problem completing your PayPal payment. Please try again:</p>
-          </h2>
-          <CtaLink
-            ctaId="become-supporter-paypal"
-            text="Become a Supporter"
-            url="https://support.theguardian.com/uk"
-            accessibilityHint="Restart your journey to become a guardian supporter"
-          />
-        </div>
-        <InfoSection heading="Questions?" className="paypal-error__questions">
-          <p>
-            If you have any questions about contributing to the Guardian,
-            please <a href="mailto:contribution.support@theguardian.com">contact us</a>
-          </p>
-        </InfoSection>
-        <InfoSection
-          heading="Spread the word"
-          className="paypal-error__spread-the-word"
-        >
-          <p>
-            We report for everyone. Let your friends and followers know that
-            you support independent journalism.
-          </p>
-          <SocialShare name="facebook" />
-          <SocialShare name="twitter" />
-        </InfoSection>
-      </div>
-    </section>
+    <ErrorIntroduction
+      highlights={['PayPal Error']}
+      headings={['Sorry, there was a', 'problem completing', 'your PayPal payment']}
+    />
+    <PageSection modifierClass="paypal-try-again">
+      <p className="try-again">Please try again.</p>
+      <CtaLink
+        text="Support The Guardian"
+        accessibilityHint="support the guardian"
+        ctaId="support-the-guardian"
+        url="/"
+      />
+    </PageSection>
+    <QuestionsContact />
     <Footer />
   </div>
 );
