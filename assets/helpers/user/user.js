289637ef732dd60da4bdbc9ca8ec7225d216be96
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2018-08-14 13:21:56 +0100

    tests pass

--- assets/helpers/user/user.js
+++ assets/helpers/user/user.js
@@ -51,11 +51,11 @@ const init = (dispatch: Function) => {
 
   if (windowHasUser) {
     dispatch(setId(window.guardian.user.id));
-    dispatch(setEmail(window.guardian.user.email.value));
+    dispatch(setEmail(window.guardian.user.email));
     dispatch(setDisplayName(window.guardian.user.displayName));
-    dispatch(setFirstName(window.guardian.user.firstName.value));
-    dispatch(setLastName(window.guardian.user.lastName.value));
-    dispatch(setFullName(`${window.guardian.user.firstName.value} ${window.guardian.user.lastName.value}`));
+    dispatch(setFirstName(window.guardian.user.firstName));
+    dispatch(setLastName(window.guardian.user.lastName));
+    dispatch(setFullName(`${window.guardian.user.firstName} ${window.guardian.user.lastName}`));
     dispatch(setIsSignedIn(true));
   } else if (userAppearsLoggedIn) {
     fetch(routes.oneOffContribAutofill, { credentials: 'include' }).then((response) => {
